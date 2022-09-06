86431910034875ab38c2e14c76d315e881ff9840
Anirudra Choudhury <53123185+AnirudraChoudhury@users.noreply.github.com>
2022-09-07 01:09:38 +0530

    Initial Album Ready

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function wedding(){
 
     mdoc.hidden=true;
     wdoc.hidden=false;
+    wdoc.style.display="grid";
     rdoc.hidden=true;
 
 }
@@ -17,5 +18,19 @@ function reception(){
     mdoc.hidden=true;
     wdoc.hidden=true;
     rdoc.hidden=false;
+    rdoc.style.display="grid";
+
+}
+
+function home(){
+    var mdoc = document.getElementById('mbody')
+    var wdoc = document.getElementById('wbody')
+    var rdoc = document.getElementById('rbody')
+
+    mdoc.hidden=false;
+    wdoc.hidden=true;
+    wdoc.style.display="None";
+    rdoc.hidden=true;
+    rdoc.style.display="None";
 
 }
