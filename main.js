c37458cfc6850b80fc590c402ca3ffa080d0678b
Anirudra Choudhury <53123185+AnirudraChoudhury@users.noreply.github.com>
2022-09-07 00:00:52 +0530

    incorporating in one page

---
+++ main.js
@@ -0,0 +1,21 @@
+function wedding(){
+    var mdoc = document.getElementById('mbody')
+    var wdoc = document.getElementById('wbody')
+    var rdoc = document.getElementById('rbody')
+
+    mdoc.hidden=true;
+    wdoc.hidden=false;
+    rdoc.hidden=true;
+
+}
+
+function reception(){
+    var mdoc = document.getElementById('mbody')
+    var wdoc = document.getElementById('wbody')
+    var rdoc = document.getElementById('rbody')
+
+    mdoc.hidden=true;
+    wdoc.hidden=true;
+    rdoc.hidden=false;
+
+}
